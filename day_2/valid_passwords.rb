f4a4723f70d4f7f85b0ade92563396108c3a8ec7
Louis Robinson <ljr5102@gmail.com>
2020-12-02 09:45:37 -0500

    solve part 2 of day 2

--- day_2/valid_passwords.rb
+++ day_2/valid_passwords.rb
@@ -9,6 +9,11 @@ class PasswordPolicy
     (int_one..int_two).include?(value.count(char))
   end
 
+  def valid_by_position?(value)
+    position_values = [value[int_one - 1], value[int_two - 1]]
+    position_values.count(char) == 1
+  end
+
   private
 
   attr_reader :char, :int_one, :int_two
@@ -16,6 +21,10 @@ end
 
 def valid_by_length_password_count(input)
   input.count { |policy, value| policy.valid_by_length?(value) }
+end
+
+def valid_by_position_password_count(input)
+  input.count { |policy, value| policy.valid_by_position?(value) }
 end
 
 def input_formatter(raw_input)
@@ -31,4 +40,5 @@ if __FILE__ == $PROGRAM_NAME
   raw_input = File.readlines("./input.txt")
   formatted = input_formatter(raw_input)
   puts "Number of valid passwords by length: #{valid_by_length_password_count(formatted)}"
+  puts "Number of valid passwords by position: #{valid_by_position_password_count(formatted)}"
 end
--- spec/day_2/valid_passwords_spec.rb
+++ spec/day_2/valid_passwords_spec.rb
@@ -2,16 +2,14 @@ require "./day_2/valid_passwords"
 
 describe PasswordPolicy do
   let(:policy) { described_class.new(char: char, int_one: int_one, int_two: int_two) }
+  let(:char) { "a" }
+  let(:int_one) { 2 }
+  let(:int_two) { 4 }
 
   describe "#valid_by_length?" do
     subject { policy.valid_by_length?(password) }
 
-    let(:char) { "a" }
-
     context "boundaries" do
-      let(:int_one) { 2 }
-      let(:int_two) { 4 }
-
       context "value does not meet minimum boundary" do
         let(:password) { "ab" }
 
@@ -43,6 +41,48 @@ describe PasswordPolicy do
       end
     end
   end
+
+  describe "#valid_by_position" do
+    subject { policy.valid_by_position?(password) }
+
+    context "password does not contain character anywhere" do
+      let(:password) { "bbbbbbb" }
+
+      it { is_expected.to be false }
+    end
+
+    context "password is shorter than positions in policy" do
+      let(:password) { "b" }
+
+      it { is_expected.to be false }
+    end
+
+    context "password contains charachter" do
+      context "character is not in either position" do
+        let(:password) { "abab" }
+
+        it { is_expected.to be false }
+      end
+
+      context "character is in position 1" do
+        let(:password) { "aaab" }
+
+        it { is_expected.to be true }
+      end
+
+      context "character is in position 2" do
+        let(:password) { "abaa" }
+
+        it { is_expected.to be true }
+      end
+
+      context "character is in position 1 and 2" do
+        let(:password) { "baba" }
+
+        it { is_expected.to be false }
+      end
+    end
+  end
 end
 
 describe "#valid_by_length_password_count" do
@@ -59,6 +99,46 @@ describe "#valid_by_length_password_count" do
       [
         instance_double(PasswordPolicy, valid_by_length?: password_1_validity),
         instance_double(PasswordPolicy, valid_by_length?: password_2_validity)
+      ]
+    end
+
+    context "no passwords are valid" do
+      let(:password_1_validity) { false }
+      let(:password_2_validity) { false }
+
+      it { is_expected.to be_zero }
+    end
+
+    context "1 password is valid" do
+      let(:password_1_validity) { true }
+      let(:password_2_validity) { false }
+
+      it { is_expected.to eq(1) }
+    end
+
+    context "both passwords are valid" do
+      let(:password_1_validity) { true }
+      let(:password_2_validity) { true }
+
+      it { is_expected.to eq(2) }
+    end
+  end
+end
+
+describe "#valid_by_position_password_count" do
+  subject { valid_by_position_password_count(input) }
+
+  context "empty input" do
+    let(:input) { [] }
+
+    it { is_expected.to be_zero }
+  end
+
+  context "non empty input" do
+    let(:input) do
+      [
+        instance_double(PasswordPolicy, valid_by_position?: password_1_validity),
+        instance_double(PasswordPolicy, valid_by_position?: password_2_validity)
       ]
     end
 
