70831788b15164cf45d75cf88a868456d3ed4e8c
Louis Robinson <ljr5102@gmail.com>
2020-12-15 09:18:07 -0500

    day 15 p 1

---
+++ day_15/game.rb
@@ -0,0 +1,37 @@
+def final_num(input)
+  result_map = {}
+  current_num = nil
+  spot = nil
+  input.each_with_index do |el, idx|
+    current_num = el
+    spot = idx + 1
+    result_map[current_num] = { val: current_num, most_recent: spot, previous: nil, times: 1 }
+  end
+
+  until spot == 2020
+    mapped_num = result_map[current_num]
+    spot += 1
+    if mapped_num && mapped_num[:times] > 1
+      current_num = mapped_num[:most_recent] - mapped_num[:previous]
+    else
+      current_num = 0
+    end
+
+    if result_map[current_num]
+      result_map[current_num][:previous] = result_map[current_num][:most_recent]
+      result_map[current_num][:most_recent] = spot
+      result_map[current_num][:times] += 1
+    else
+      result_map[current_num] = { val: current_num, most_recent: spot, previous: nil, times: 1 }
+    end
+  end
+
+  current_num
+end
+
+if __FILE__ == $PROGRAM_NAME
+  input = [16,11,15,0,1,7]
+  final = final_num(input)
+
+  puts "The 2020th number in the game is: #{final}"
+end
---
+++ spec/day_15/game_spec.rb
@@ -0,0 +1,21 @@
+require "./day_15/game"
+
+describe "#final_num" do
+  subject { final_num(input) }
+
+  TEST_1_CONFIG = [
+    { input: [0, 3, 6], output: 436 },
+    { input: [1, 3, 2], output: 1 },
+    { input: [2, 1, 3], output: 10 },
+    { input: [1, 2, 3], output: 27 },
+    { input: [2, 3, 1], output: 78 },
+    { input: [3, 2, 1], output: 438 },
+    { input: [3, 1, 2], output: 1836 },
+  ].each do |config|
+    context "for input: #{config[:input]}" do
+      let(:input) { config[:input] }
+
+      it { is_expected.to eq(config[:output]) }
+    end
+  end
+end
