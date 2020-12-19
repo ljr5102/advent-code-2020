700990b860a01950a61062c9893822aff03f1724
Louis Robinson <ljr5102@gmail.com>
2020-12-18 19:08:26 -0500

    day 18 part 2 -quite ugly :0

--- day_18/math.rb
+++ day_18/math.rb
@@ -30,6 +30,42 @@ def evaluate(expression)
   result
 end
 
+def evaluate_v2(expression)
+  result = 0
+  operator = "+"
+  expression = expression.gsub(" ", "")
+  idx = 0
+  while idx < expression.length
+    char = expression[idx]
+    if char == "+"
+      operator = char
+      idx += 1
+    elsif char == "*"
+      remaining = expression[(idx + 1)..]
+      result = operate("*", result, evaluate_v2(remaining))
+      idx = expression.length
+    elsif char == "("
+      nesting = 1
+      nested_expression = ""
+      nested_idx = idx + 1
+      nested_char = expression[nested_idx]
+      until nesting == 0
+        nesting += 1 if nested_char == "("
+        nesting -= 1 if nested_char == ")"
+        nested_expression << nested_char unless nesting == 0
+        nested_idx += 1
+        nested_char = expression[nested_idx]
+      end
+      result = operate(operator, result, evaluate_v2(nested_expression))
+      idx = nested_idx
+    else
+      result = operate(operator, result, char.to_i)
+      idx += 1
+    end
+  end
+  result
+end
+
 def operate(char, num1, num2)
   if char == "+"
     num1 + num2
@@ -44,6 +80,8 @@ if __FILE__ == $PROGRAM_NAME
   input = File.readlines("./input.txt").map(&:strip)
 
   sum = input.sum { |expression| evaluate(expression) }
+  sum_v2 = input.sum { |expression| evaluate_v2(expression) }
 
   puts "The sum of each expression result is #{sum}"
+  puts "The sum of each expression version 2 result is #{sum_v2}"
 end
--- spec/day_18/math_spec.rb
+++ spec/day_18/math_spec.rb
@@ -18,3 +18,22 @@ describe "#evaluate" do
     end
   end
 end
+
+describe "#evaluate_v2" do
+  subject { evaluate_v2(expression) }
+
+  TEST_CONFIG = [
+    { expression: "1 + 2 * 3 + 4 * 5 + 6 ", result: 231 },
+    { expression: "1 + (2 * 3) + (4 * (5 + 6))", result: 51 },
+    { expression: "2 * 3 + (4 * 5)", result: 46 },
+    { expression: "5 + (8 * 3 + 9 + 3 * 4 * 3)", result: 1445 },
+    { expression: "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", result: 669060 },
+    { expression: "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", result: 23340 },
+  ].each do |config|
+    context "for expression #{config[:expression]}" do
+      let(:expression) { config[:expression] }
+
+      it {is_expected.to eq(config[:result]) }
+    end
+  end
+end
